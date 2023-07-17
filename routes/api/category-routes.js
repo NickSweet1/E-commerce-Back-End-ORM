99bbfc017e03e6fe775b776ef6854d0bdd62d34f
Nick <nicksweetj@gmail.com>
2023-07-17 19:29:54 -0400

    added the first api get request

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,9 +3,15 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  const allCategories = await Category.findAll({
+    include: [{ model: Product }], // Include associated Products
+  }).catch((err) => {
+    res.json(err);
+  });
+  res.json(allCategories);
 });
 
 router.get('/:id', (req, res) => {
