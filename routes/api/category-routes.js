a59e8c4aa79379556a2e8894348b004833dd4d3b
Nick <nicksweetj@gmail.com>
2023-07-17 20:56:16 -0400

    category-routes.js is finished. the delete route may need so reworking

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -14,21 +14,51 @@ router.get('/', async (req, res) => {
   res.json(allCategories);
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
-});
+  const categoryById = await Category.findByPk(req.params.id, {
+    include: [{ model: Product }],
+  });
 
-router.post('/', (req, res) => {
+  if (!categoryById) {
+    return res.status(404).json({ message: 'Category does not exist'});
+  }
+  res.json(categoryById);
+  } 
+);
+
+router.post('/', async (req, res) => {
   // create a new category
+  const newCategory = await Category.create({
+    category_name: req.body.category_name,
+  })
+  res.status(201).json(newCategory);
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  const categoryById = await Category.update({
+    id: req.body.id,
+  },
+  {
+    where: {
+      id: req.params.id,
+    }
+  })
+  res.status(200).json(categoryById)
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  await Category.destroy({
+    where: {
+      id: req.params.id,
+    }
+  })
+  .then((deletedCategory) => {
+    res.json(deletedCategory);
+  })
 });
 
 module.exports = router;
